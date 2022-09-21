9bd8b32668e475a99cd6134eb28de62bd9e0e692
Charles Moulliard <cmoulliard@redhat.com>
2022-09-21 18:57:24 +0200

    Creating the ingress as part of the cluster1. WIP. We should find a way to create it part of the workspace namespace

--- skupper/scenario-2.sh
+++ skupper/scenario-2.sh
@@ -121,6 +121,5 @@ pe "KUBECONFIG=${TEMP_DIR}/${KCP_CFG_PATH} k apply -f ./k8s/bookinfo_one_skupper
 pe "KUBECONFIG=${TEMP_DIR}/${KCP_CFG_PATH} k apply -f ./k8s/bookinfo_two_skupper.yaml -n two"
 
 p "Expose the bookinfo as ingress route to access it externally on the cluster1"
-pe "k ctx kind-cluster1"
-pe "k create ingress bookinfo --class=nginx --rule=\"bookinfo.${HOST_MACHINE}/*=productpage:9080\" -n one"
+pe "k create ingress bookinfo --class=nginx --rule=\"bookinfo.${HOST_MACHINE}/*=productpage:9080\""
 
